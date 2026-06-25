08dee2ce02d6a26d1da54bd0ae27b66cb4b248ed
ingilapallideena <ingilapallideena1@gmail.com>
2026-06-25 19:18:44 +0530

    Add files via upload

---
+++ nested list.py
@@ -0,0 +1,7 @@
+# CONVERT BINARY TO DECIMAL
+binary = "1011"
+decimal = 0
+for i in range(len(binary)):
+    digit = int(binary[i])
+    decimal += digit * (2 ** (len(binary) - i - 1))
+print(decimal)
