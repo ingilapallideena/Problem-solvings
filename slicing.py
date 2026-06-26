30fb071c613d65affc11161f02c1a59b32acca82
ingilapallideena <ingilapallideena1@gmail.com>
2026-06-26 16:07:47 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,14 @@
+# 1.Atleast 10 different output generate in slicing  in 
+text = "rishika"
+print( text[:])       # rishika
+print( text[:3])      # ris
+print( text[-3:])     #  ika
+print( text[2:5])     #  shi
+print( text[::2])     #  rsia
+print( text[::-1])    #  akihsir
+print( text[:5])      #  rishi
+print( text[-1])      #  a
+print( text[1:])      #  ishika
+print( text[1:-1])    #  ishik
+print( text[::3])     #  ria
+print( text[::-2])     
