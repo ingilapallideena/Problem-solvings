435e5899613faf1342ab149d73c23326833a31fa
ingilapallideena <ingilapallideena1@gmail.com>
2026-06-26 16:16:47 +0530

    Add files via upload

---
+++ bitwise.py
@@ -0,0 +1,11 @@
+# Bitwise Operators
+a = 5
+b = 3
+print("a =", a)
+print("b =", b)
+print("AND (&)       :", a & b)
+print("OR (|)        :", a | b)
+print("XOR (^)       :", a ^ b)
+print("NOT (~a)      :", ~a)
+print("Left Shift    :", a << 1)
+print("Right Shift   :", a >> 1)
