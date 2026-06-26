726920c402b88bee12feb1ef2c5d25bd73990b6a
ingilapallideena <ingilapallideena1@gmail.com>
2026-06-26 16:13:12 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,17 @@
+# All Assignment Operators
+a = 10
+print("Initial value:", a)
+a += 5
+print("After += :", a)
+a -= 3
+print("After -= :", a)
+a *= 2
+print("After *= :", a)
+a /= 4
+print("After /= :", a)
+a //= 2
+print("After //= :", a)
+a %= 3
+print("After %= :", a)
+a **= 3
+print("After **= :", a)
