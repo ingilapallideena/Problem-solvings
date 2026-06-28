e861bbec2f871c693d48a53c4323261207b1df3c
ingilapallideena <ingilapallideena1@gmail.com>
2026-06-28 16:49:12 +0530

    Add files via upload

---
+++ io error.py
@@ -0,0 +1,6 @@
+# Input and Output Function Errors
+name = input(Enter your name: )      
+age = input("Enter your age: ")
+print(age + 5)                       
+pritn("Welcome", name)               
+print("Your age is", age)         
