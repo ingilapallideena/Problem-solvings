e68f5d1c371ea1f88c4a0f4a354a80cae7d91021
ingilapallideena <ingilapallideena1@gmail.com>
2026-06-28 16:23:51 +0530

    Add files via upload

---
+++ formulas.py
@@ -0,0 +1,79 @@
+# Square
+a = 5
+square = a ** 2
+print(square)
+# Cube
+a = 4
+cube = a ** 3
+print(cube)
+# Square Root
+import math
+a = 49
+print(math.sqrt(a))
+# Cube Root
+a = 125
+print(a ** (1/3))
+# Average
+a = 20
+b = 30
+c = 40
+avg = (a + b + c) / 3
+print(avg)
+# Percentage
+obtained = 420
+total = 500
+percentage = (obtained / total) * 100
+print(percentage)
+# Simple Interest
+P = 10000
+R = 5
+T = 2
+SI = (P * R * T) / 100
+print(SI)
+# Compound Interest
+P = 10000
+R = 5
+T = 2
+CI = P * (1 + R / 100) ** T - P
+print(CI)
+# Area of Rectangle
+l = 10
+b = 5
+area = l * b
+print(area)
+# Perimeter of Rectangle
+l = 10
+b = 5
+perimeter = 2 * (l + b)
+print(perimeter)
+# Area of Square
+side = 6
+print(side * side)
+# Perimeter of Square
+side = 6
+print(4 * side)
+# Area of Circle
+side = 6
+print(side * side)
+# Perimeter of Square
+side = 6
+print(4 * side)
+# Area of Circle
+import math
+r = 7
+area = math.pi * r ** 2
+print(area)
+# Circumference of Circle
+import math
+r = 7
+print(2 * math.pi * r)
+# Area of Triangle
+base = 10
+height = 8
+area = 0.5 * base * height
+print(area)
+# Perimeter of Triangle
+a = 5
+b = 6
+c = 7
+print(a + b + c)
